2da578e01eaa2b7cea3ee3826ab9a9c1a85cf9e0
Raphael Roullet <10075759+ra-phael@users.noreply.github.com>
2021-08-07 16:57:31 +0100

    add did packages

--- lambda/vc-issuer/app.js
+++ lambda/vc-issuer/app.js
@@ -1,5 +1,10 @@
 // const axios = require('axios')
 // const url = 'http://checkip.amazonaws.com/';
+const { DID } = require("dids");
+const KeyResolver = require("key-did-resolver");
+
+const did = new DID({ resolver: KeyResolver.default.getResolver() });
+
 let response;
 
 /**
@@ -7,27 +12,27 @@ let response;
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 exports.lambdaHandler = async (event, context) => {
-    try {
-        // const ret = await axios(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world',
-                // location: ret.data.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  try {
+    // const ret = await axios(url);
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "hello world",
+        // location: ret.data.trim()
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 
-    return response
+  return response;
 };
